a2de8185511058d119ebacffb5dc95ab6e7432f6
Txomin <tmedrano@tarragona.cat>
2023-03-27 13:30:28 +0200

    added isjson helper

--- src/Helpers/json.php
+++ src/Helpers/json.php
@@ -1,5 +1,19 @@
 <?php
 
+if (! function_exists('isJson')) {
+	function isJson($string) {
+	 	try{
+			if(!is_string($string)) return false;
+			$ret=json_decode($string);
+			if(!is_array($ret) && !is_object($ret)) return false; //es un tipo simple
+			 
+			return (json_last_error() == JSON_ERROR_NONE);
+		}catch(Exception $e){
+			return false;
+		}
+	}
+}
+
 if (! function_exists('to_object')) {
 	function to_object($array) {
 	 	return json_decode(json_encode($array), FALSE);
